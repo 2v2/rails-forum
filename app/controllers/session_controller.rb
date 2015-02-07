b2b7555b2e5730965be8e2cdf503ab7c2e54238e
DavideDaniel <davidedaniel@gmail.com>
2015-02-07 17:54:04 -0500

    working routing actions for log in out from same page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,20 +1,24 @@
 class SessionController < ApplicationController
-	def new
-		render :new
-	end
+  def new
+    if !session[:user_id]
+      render :new
+    else
+      redirect_to session[:page_id]
+    end
+  end
 
-	def create
-		user = User.find_by(screen_name: params[:screen_name])
-		if user && user.authenticate(params[:password])
-			session[:user_id] = user.id
-			redirect_to '/topics'
-		else
-			render :new
-		end
-	end
+  def create
+    user = User.find_by(screen_name: params[:screen_name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to session[:page_id]
+    else
+      render :new
+    end
+  end
 
-	def destroy 
-		reset_session
-		redirect_to '/topics'
-	end
+  def destroy
+    reset_session
+    redirect_to :back
+  end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,15 @@
 class TopicsController < ApplicationController
 
   def index
-    
+    session[:page_id] = '/'
     @user = User.find_by(id: session[:user_id])
     @topics = Topic.all()
   end
 
   def show
+  	@user = User.find_by(id: session[:user_id])
     @topic = Topic.find_by(id: params[:id])
+    session[:page_id] = topic_path(@topic)
   end
 
 end
