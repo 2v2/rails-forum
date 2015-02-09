9b69f09a177e3e5424740d8e838bafbe0dc95353
DavideDaniel <davidedaniel@gmail.com>
2015-02-09 18:09:37 -0500

    switching to css vs gem

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,27 +10,30 @@ class TopicsController < ApplicationController
     @page = params["page"] || 1
     @page = @page.to_i
 
-  	@user = User.find_by(id: session[:user_id])
+    @user = User.find_by(id: session[:user_id])
     @topic = Topic.find_by(id: params[:id])
     count = 1
-    
+
     all_entries = @topic.entries
+
     @pages = (all_entries.length/10) + 1
-    if @page < 1 || @pages < @page
 
-      @page = @pages
-
+    if @page < 1
+      @page = 1
     end
+    if @pages < @page
+      @page = @pages
+    end
+
     all_entries.in_groups_of(10) do |group|
       if count == @page
         @entries = group
       end
       count+=1
     end
-    
+
     session[:page_id] = topic_path(@topic) + "?page=" + @page.to_s
 
   end
 
 end
-
