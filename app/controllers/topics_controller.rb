bc554b31e5c88480c1eaee868bf3b20c24abd0ed
Alexander Deschamps <alexadeschamps@gmail.com>
2015-02-09 14:57:51 -0500

    pagination started/working

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,8 +7,21 @@ class TopicsController < ApplicationController
   end
 
   def show
+    page = params["page"] || 1
+    page = page.to_i
+    binding.pry
   	@user = User.find_by(id: session[:user_id])
     @topic = Topic.find_by(id: params[:id])
+    count = 1
+    @topic.entries.in_groups_of(10) do |group|
+      puts group
+      if count == page
+        binding.pry
+        @entries = group
+      end
+      count+=1
+    end
+    
     session[:page_id] = topic_path(@topic)
   end
 
