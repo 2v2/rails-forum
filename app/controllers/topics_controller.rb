a125e24622148f80e402d5e5529aea5e6fc2992b
Alexander Deschamps <alexadeschamps@gmail.com>
2015-02-09 19:18:02 -0500

    fixed pagination somewhat

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,30 +7,28 @@ class TopicsController < ApplicationController
   end
   
   def show
-    @page = params["page"] || 1
-    @page = @page.to_i
-
+    page = params["page"] || 1
+    page = page.to_i
     @user = User.find_by(id: session[:user_id])
     @topic = Topic.find_by(id: params[:id])
     count = 1
-
     all_entries = @topic.entries
-
     @pages = (all_entries.length/10) + 1
-
-    if @page < 1
-      @page = 1
+    if page < 1
+      page = 1
     end
-    if @pages < @page
-      @page = @pages
+    if @pages < page
+      page = @pages
     end
-
+    entries = []
     all_entries.in_groups_of(10) do |group|
-      if count == @page
-        @entries = group
+      if count == page
+        entries = group
       end
       count+=1
     end
+    @page = page
+    @entries = entries
 
     session[:page_id] = topic_path(@topic) + "?page=" + @page.to_s
 
