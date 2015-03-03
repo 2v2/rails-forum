5a325516172eb61ff00ef1db0aaea3f1180a1f7f
Alex <alexadeschamps@gmail.com>
2015-03-03 15:00:40 -0500

    changed seed

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,7 +9,6 @@ class SessionController < ApplicationController
 
   def create
     user = User.find_by(screen_name: params[:screen_name])
-    binding.pry
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to session[:page_id]
