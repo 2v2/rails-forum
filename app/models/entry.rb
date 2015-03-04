46b662f5242669471e193f8a76e60eb031d0f4d8
DavideDaniel <davidedaniel@gmail.com>
2015-03-03 20:45:03 -0500

    new migration and edits to model for entry_tags

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,5 +3,11 @@ class Entry < ActiveRecord::Base
   belongs_to :topic
 
   has_many :comments
-  has_many :entryTags
+  has_many :entry_tags, dependent: :destroy
+  has_many :viewers, through: :entry_tags, source: :user
+
+  	def is_tagged?(user)
+    true if user.id == self.user_id
+    entry_tags.where(user_id: user.id).exists?
+  end
 end
